689abbdea40d9c161682a3b2a29bc0e8f7fb54c7
Alexander Kondrakhin <a.kondrakhin@gmail.com>
2015-03-14 19:50:20 +0100

    Tabs rplaced with spaces

--- long_math_lib/LongMath.cpp
+++ long_math_lib/LongMath.cpp
@@ -2,11 +2,11 @@
 
 std::ostream & operator<<(std::ostream & os, LongMath const & lm)
 {
-	if (!lm.value.empty())
-	{
-		char sign =  (lm.sign == LongMath::Sign::NEG) ? '-' : '+';  			
-		os << sign;
-	}
+    if (!lm.value.empty())
+    {
+        char sign =  (lm.sign == LongMath::Sign::NEG) ? '-' : '+';              
+        os << sign;
+    }
         auto it = lm.value.rbegin(), it_end = lm.value.rend();
         for(; it != it_end; ++it)
         {
