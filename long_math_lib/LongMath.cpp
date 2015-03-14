17a76791f03ef42698ebe823dda70717d5c8e2db
Alexander Kondrakhin <a.kondrakhin@gmail.com>
2015-03-14 16:39:26 +0100

    basic stuff

---
+++ long_math_lib/LongMath.cpp
@@ -0,0 +1,17 @@
+#include "LongMath.h"
+
+std::ostream & operator<<(std::ostream & os, LongMath const & lm)
+{
+	if (!lm.value.empty())
+	{
+		char sign =  (lm.sign == LongMath::Sign::NEG) ? '-' : '+';  			
+		os << sign;
+	}
+        auto it = lm.value.rbegin(), it_end = lm.value.rend();
+        for(; it != it_end; ++it)
+        {
+                os << (char)(*it + 48);
+        }
+        return os;
+}
+
---
+++ long_math_test/test.cpp
@@ -0,0 +1,91 @@
+#include <gtest/gtest.h>
+#include <ostream>
+#include <fstream>
+
+#include "LongMath.h"
+
+TEST(LongMath, DefaultValue) 
+{
+	LongMath l;
+	EXPECT_EQ(l, 0);
+}
+
+TEST(LongMath, InitValue)
+{
+	LongMath l(5);
+	EXPECT_EQ(l, 5);
+}
+
+TEST(LongMath, InitTwoDigitValue)
+{
+	LongMath l(1);
+	EXPECT_EQ(l, 1);
+}
+
+TEST(LongMath, InitTwoDigitValue2)
+{
+	LongMath l(12);
+	EXPECT_EQ(l, 12);
+
+	LongMath l1(0);
+	EXPECT_EQ(l1, 0);
+}
+
+TEST(LongMath, InitTwoDigitNegativeValue)
+{
+	LongMath l(-12);
+	EXPECT_EQ(l, -12);
+}
+
+TEST(LongMath, Dump)
+{
+	LongMath l(-12);
+	std::ostringstream oss;
+	
+	oss << l;
+
+	EXPECT_EQ(oss.str(), "-12");
+}
+
+
+TEST(LongMath, Addition)
+{
+	LongMath l(5), k(2);
+
+	EXPECT_EQ(l+k, 7);
+}
+
+TEST(LongMath, TwoDigitAddition)
+{
+	LongMath l(2), k(199);
+
+	EXPECT_EQ(l+k, 201); 
+	
+	std::cout << "L+K=" << l+k << std::endl;
+}
+
+TEST(LongMath, InitFromString)
+{
+	std::string s = "-12563";
+	std::string s2 = "1563";
+
+	EXPECT_EQ(LongMath(s), -12563);
+	EXPECT_EQ(LongMath(s2), 1563);
+}
+
+TEST(LongMath, ReallyLong)
+{
+	std::ifstream f("input.txt");
+	std::string line;
+	
+	LongMath res;
+ 
+	while (std::getline(f, line))
+	{
+		LongMath l(line);
+		res = res + l;
+	}
+	
+	std::cout << "res=[" << res << "]"  << std::endl;
+}	
+
