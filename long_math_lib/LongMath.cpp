7c3fe8738645f72c65c397421e5fa7616dd1afcd
pierre <p.monjallon@gmail.com>
2015-03-16 20:01:24 +0100

    Refactoring of LongMath.h

--- long_math_lib/LongMath.cpp
+++ long_math_lib/LongMath.cpp
@@ -1,5 +1,62 @@
 #include "LongMath.h"
 
+// ******************************  public  ********************************
+
+LongMath LongMath::operator+ (const LongMath & lm) const
+{
+    LongMath new_lm(*this);
+    
+    const auto size_l = new_lm.value.size();
+    const auto size_r = lm.value.size();
+    const auto max_size = std::max(size_l, size_r);
+    int carry = 0;
+
+    // TODO : If either this or lm is negative
+    
+    for (auto i = 0; i < max_size || carry > 0; ++i)
+    {
+        const auto left  = (i < size_l) ? new_lm.value[i] : 0;
+        const auto right = (i < size_r) ? lm.value[i] : 0; 
+        
+        const div_t result  = div(left + right + carry, 10 );
+                
+        if (i < size_l)
+        {
+            new_lm.value[i] = result.rem;
+        }
+        else
+        {
+            new_lm.value.push_back(result.rem);
+        }
+
+        carry = result.quot;
+    }
+
+    return new_lm;    
+}
+
+
+LongMath LongMath::operator* (const LongMath & right_factor) const
+{
+    const LongMath left_factor(*this);
+    LongMath result;
+
+    if (value.size() > TRIGGER_STRASSEN)
+    {
+        result = strassenMultiplication(left_factor, right_factor);
+    }
+    else if (value.size() > TRIGGER_KARATSUBA)
+    {
+        result = karatsubaMultiplication(left_factor, right_factor);
+    }
+    else
+    {
+        result = standardMultiplication(left_factor, right_factor);
+    }
+
+    return result;
+}
+
 std::ostream & operator<<(std::ostream & os, LongMath const & lm)
 {
     if (!lm.value.empty())
@@ -15,3 +72,113 @@ std::ostream & operator<<(std::ostream & os, LongMath const & lm)
         return os;
 }
 
+// ******************************  private  ********************************
+
+const size_t LongMath::TRIGGER_STRASSEN;
+const size_t LongMath::TRIGGER_KARATSUBA;
+
+
+
+LongMath LongMath::standardMultiplication(const LongMath & left_factor, const LongMath & right_factor) const
+{
+    LongMath result(0);
+
+    unsigned int index = 0;
+
+    for (; index < left_factor.value.size(); ++index)
+    {
+        LongMath tmp = right_factor * left_factor.value[index];
+        tmp.shift(index);
+
+        result = result + tmp;
+    }
+
+    return result;
+}
+
+LongMath LongMath::strassenMultiplication(const LongMath & left_factor, const LongMath & right_factor) const
+{
+    return left_factor;
+}
+
+LongMath LongMath::karatsubaMultiplication(const LongMath & left_factor, const LongMath & right_factor) const
+{
+    return left_factor;
+
+    /*
+    if (left_factor.value.size() < 2 || right_factor.value.size() < 2)
+        return standardMultiplication(left_factor, right_factor);
+
+    const size_t middle_point = min(left_factor.value.size(), right_factor.value.size()) / 2;
+    */
+}
+
+LongMath & LongMath::shift(int power)
+{
+    // TODO
+    return *this;
+}
+
+LongMath LongMath::splitAt(size_t index) const
+{
+    LongMath splitted(*this);
+
+    // TODO
+    return splitted;
+}
+
+
+LongMath LongMath::operator* (int right_factor) const
+{
+    const LongMath left_factor(*this);
+    
+    // TODO
+
+    return left_factor;   
+}
+
+void LongMath::setFromInt(int val)
+{
+    if (val < 0)
+    {
+        val = -val;
+        sign = Sign::NEG;
+    }
+    else
+    { 
+        sign = Sign::POS;
+    }
+        
+    div_t result = div(val, 10);
+    int i = 0;
+    
+    while (result.quot != 0 || result.rem != 0)
+    {
+        value.push_back(result.rem);
+        result = div(result.quot, 10);
+    }
+}
+
+void LongMath::setFromString(std::string const & val)
+{
+    if (val.empty())
+        return;
+
+    auto it = val.rbegin();
+    auto it_end = val.rend();
+    
+    if (val[0] == '-')
+    {
+        sign = Sign::NEG;
+        it_end--;
+    }
+    else
+    {
+        sign = Sign::POS;
+    }
+
+    for (; it != it_end; ++it)
+    {
+        value.push_back(*it - 48);
+    }
+}
--- long_math_lib/LongMath.h
+++ long_math_lib/LongMath.h
@@ -42,169 +42,30 @@ public:
         return (*this) == other;
     }
 
-    LongMath operator+ (const LongMath & lm) const
-    {
-        LongMath new_lm(*this);
-        
-        const auto size_l = new_lm.value.size();
-        const auto size_r = lm.value.size();
-        const auto max_size = std::max(size_l, size_r);
-        int carry = 0;
-
-        // TODO : If either this or lm is negative
-        
-        for (auto i = 0; i < max_size || carry > 0; ++i)
-        {
-            const auto left  = (i < size_l) ? new_lm.value[i] : 0;
-            const auto right = (i < size_r) ? lm.value[i] : 0; 
-            
-            const div_t result  = div(left + right + carry, 10 );
-                    
-            if (i < size_l)
-            {
-                new_lm.value[i] = result.rem;
-            }
-            else
-            {
-                new_lm.value.push_back(result.rem);
-            }
-
-            carry = result.quot;
-        }
-
-        return new_lm;    
-    }
-
-    LongMath operator* (int right_factor) const
-    {
-        const LongMath left_factor(*this);
-        
-        // TODO
-
-        return left_factor;   
-    }
-
-    LongMath operator* (const LongMath & right_factor) const
-    {
-        const LongMath left_factor(*this);
-        LongMath result;
-
-        if (value.size() > TRIGGER_STRASSEN)
-        {
-            result = strassenMultiplication(left_factor, right_factor);
-        }
-        else if (value.size() > TRIGGER_KARATSUBA)
-        {
-            result = karatsubaMultiplication(left_factor, right_factor);
-        }
-        else
-        {
-            result = standardMultiplication(left_factor, right_factor);
-        }
-
-        return result;
-    }
-
-    LongMath strassenMultiplication(const LongMath & left_factor, const LongMath & right_factor) const
-    {
-        return left_factor;
-    }
-
-    LongMath karatsubaMultiplication(const LongMath & left_factor, const LongMath & right_factor) const
-    {
-        if (left_factor.value.size() < 2 || right_factor.value.size() < 2)
-            return standardMultiplication(left_factor, right_factor);
-
-        const size_t middle_point = min(left_factor.value.size(), right_factor.value.size()) / 2;
-
-        
-
-    }
-
-    LongMath standardMultiplication(const LongMath & left_factor, const LongMath & right_factor) const
-    {
-        LongMath result(0);
-
-        unsigned int index = 0;
-
-        for (; index < left_factor.size(); ++index)
-        {
-            LongMath tmp = right_factor * left_factor[index];
-            tmp.shift(index);
-
-            result = result + tmp;
-        }
-
-        return result;
-    }
-
-    LongMath & shift(int power)
-    {
-        // TODO
-        return *this;
-    }
-
-    LongMath splitAt(size_t index) const
-    {
-        LongMath splitted(*this);
-
-        // TODO
-        return splitted;
-    }
-
+    LongMath operator+ (const LongMath & lm) const;
+    LongMath operator* (const LongMath & right_factor) const;
 
     bool isNegative() const { return sign == Sign::NEG; }
 
     friend std::ostream & operator<<(std::ostream &, LongMath const &);
 
-private:
-    void setFromInt(int val)
-    {
-        if (val < 0)
-        {
-            val = -val;
-            sign = Sign::NEG;
-        }
-        else
-        { 
-            sign = Sign::POS;
-        }
-            
-        div_t result = div(val, 10);
-        int i = 0;
-        
-        while (result.quot != 0 || result.rem != 0)
-        {
-            value.push_back(result.rem);
-            result = div(result.quot, 10);
-        }
-    }
-
-    void setFromString(std::string const & val)
-    {
-        if (val.empty())
-            return;
-
-        auto it = val.rbegin();
-        auto it_end = val.rend();
-        
-        if (val[0] == '-')
-        {
-            sign = Sign::NEG;
-            it_end--;
-        }
-        else
-        {
-            sign = Sign::POS;
-        }
-
-        for (; it != it_end; ++it)
-        {
-            value.push_back(*it - 48);
-        }
-    }
 
 private:
+    LongMath operator* (int right_factor) const;
+    LongMath strassenMultiplication(const LongMath & left_factor, const LongMath & right_factor) const;
+    LongMath karatsubaMultiplication(const LongMath & left_factor, const LongMath & right_factor) const;
+    LongMath standardMultiplication(const LongMath & left_factor, const LongMath & right_factor) const;
+
+    LongMath & shift(int power);
+    LongMath splitAt(size_t index) const;
+
+    void setFromInt(int val);
+    void setFromString(std::string const & val);
+
+private:
+    static const size_t TRIGGER_STRASSEN = 100;
+    static const size_t TRIGGER_KARATSUBA = 10;
+
     Buffer value;
     Sign   sign;
 };
